674a21645406d8ec081ac7e9c6e7d1bbd3a5b486
ialinaok <alinapielaszkiewicz@gmail.com>
2022-02-11 00:17:09 +0100

    added strlcpy

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apielasz <apielasz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 17:42:46 by ialinaok          #+#    #+#             */
-/*   Updated: 2022/02/10 23:29:25 by apielasz         ###   ########.fr       */
+/*   Updated: 2022/02/11 00:15:21 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,31 @@ char	*ft_strchr(char const *str, int c)
 	if (*ptr == c) // this is in case c == '\0', it's not needed for gnl but whatever
 		return (ptr);
 	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
+{
+	size_t	src_len;
+	int		n;
+
+	src_len = ft_strlen(src);
+	n = src_len;
+	if (src_len + 1 < dst_size)
+	{
+		while ((n + 1) != 0)
+		{
+			*dst++ = *src++;
+			n--;
+		}
+	}
+	if ((src_len >= dst_size) && (dst_size != 0))
+	{
+		while ((dst_size - 1) != 0)
+		{
+			*dst++ = *src++;
+			dst_size--;
+		}
+		dst[dst_size - 1] = '\0';
+	}
+	return (src_len);
 }
